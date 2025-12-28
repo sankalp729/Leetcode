0812d2393de47d27679b26d267830a11812bab81
sankalp729 <sankalp7479@gmail.com>
2025-12-28 21:44:54 +0530

    Time: 373 ms (33.70%) | Memory: 42.9 MB (42.06%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -3,12 +3,12 @@ class Solution {
         backtrack(board);
     }
     public boolean backtrack(char[][] board){
-        for(int i =0; i<board.length; i++){
+        for(int i = 0; i<board.length; i++){
             for(int j =0; j<board[0].length; j++){
                 if(board[i][j] == '.'){
-                    for(char c = '1'; c<='9'; c++){
-                        if(isSafe(board, i, j, c)){
-                            board[i][j] = c;
+                    for(char ch = '1'; ch<='9'; ch++){
+                        if(isSafe(board, i, j, ch) == true){
+                            board[i][j] = ch;
                             if(backtrack(board) == true) return true;
                             else board[i][j] = '.';
                         }
@@ -23,7 +23,7 @@ class Solution {
         for(int i =0; i<9; i++){
             if(board[row][i] == c) return false;
             else if(board[i][col] == c) return false;
-            else if(board[3*(row/3) + i/3][3*(col/3) + i%3] == c) return false;
+            else if(board[3*(row/3) + i/3][3*(col/3) + i%3] == c) return false; 
         }
         return true;
     }
