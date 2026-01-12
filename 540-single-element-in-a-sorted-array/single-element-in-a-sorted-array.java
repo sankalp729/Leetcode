6eb4cba9f67d69c8e05899a690593509fca12e90
sankalp729 <sankalp7479@gmail.com>
2026-01-12 17:39:50 +0530

    Time: 0 ms (100.00%) | Memory: 52.9 MB (63.00%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,21 +1,19 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        // edge cases :
         int n = nums.length;
         
-        if(nums.length == 1) return nums[0];
+        //edge cases
+        if(n == 1) return nums[0];
         if(nums[0] != nums[1]) return nums[0];
         if(nums[n-1] != nums[n-2]) return nums[n-1];
-        
-        int low =1, high = n-2;
 
+        int low = 1, high = n-2;
         while(low<=high){
             int mid = (low + high)/2;
             if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
-            else if((mid%2 == 0 && nums[mid] == nums[mid+1]) || (mid%2 == 1 && nums[mid] == nums[mid-1])) low = mid+1;
+            else if((mid%2 ==0 && nums[mid] == nums[mid+1]) || (mid%2 == 1 && nums[mid] == nums[mid-1])) low = mid+1;
             else high = mid-1;
         }
-
         return -1;
     }
 }
