c2ce3c2a7728ee05c153ad9116d838b2a13fbbe1
sankalp729 <sankalp7479@gmail.com>
2026-03-05 19:52:42 +0530

    Time: 2 ms (90.23%) | Memory: 47.8 MB (56.21%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,18 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i =0, j= 0;
-        for(int k = 0; k<nums.length; k++){
-            if(k<nums.length - 1 && nums[k] == 0){
-                j = k;
-                i = k+1;
+        // step 1 : determine the index of the first zero : 
+        int i =0, j =0;
+        for(int k =0; k<nums.length; k++){
+            if(nums[k] == 0){
+                j =k;
+                i =k+1;
                 break;
-            } 
+            }
         }
         while(i<nums.length){
             if(nums[i] != 0){
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
                 j++;
             }
             i++;
