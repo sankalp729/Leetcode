f6ca51cdacfafc162559983ef96ffd5bad0b8a3f
sankalp729 <sankalp7479@gmail.com>
2026-03-07 17:35:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (73.22%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,19 +1,18 @@
 class Solution {
     public int findMin(int[] nums) {
-        // identify the sorted half and replace its first element with the minEl.
-        int n = nums.length;
-        int minEl = Integer.MAX_VALUE;
-        int low =0, high = n-1;
+        // find the sorted half, take its minimum and eliminate that half!
+        int low = 0, high = nums.length-1;
+        int ans = Integer.MAX_VALUE;
         while(low<=high){
             int mid = (low + high)/2;
-            if(nums[low] <= nums[mid]){
-                minEl = Math.min(minEl, nums[low]);
+            if(nums[low]<=nums[mid]){
+                ans = Math.min(ans, nums[low]);
                 low = mid+1;
             }else{
-                minEl = Math.min(minEl, nums[mid]);
+                ans = Math.min(ans, nums[mid]);
                 high = mid-1;
             }
         }
-        return minEl; 
+        return ans;
     }
 }
