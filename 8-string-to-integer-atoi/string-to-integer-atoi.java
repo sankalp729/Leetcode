484c16f43faeef4c7c8b308766f4b98f2bb02660
sankalp729 <sankalp7479@gmail.com>
2026-03-22 14:09:54 +0530

    Time: 1 ms (99.88%) | Memory: 43.5 MB (96.50%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,23 +1,25 @@
 class Solution {
     public int myAtoi(String s) {
-        int i =0;
-        int sign = 1;
-        long res = 0;
-        while(i<s.length() && s.charAt(i) == ' ') i++;
-        if(i<s.length() && s.charAt(i) == '-'){
-            sign = -1;
+        long ans= 0;
+        int i=0;
+        int sign= 1;
+        int n= s.length();
+        while(i<n && s.charAt(i)== ' '){
             i++;
         }
-        else if(i<s.length() && s.charAt(i) == '+'){
+        if(i<n && s.charAt(i) == '-'){
+            sign = -1;
+            i++;
+        }else if(i<n && s.charAt(i) == '+'){
             sign = 1;
             i++;
         }
-        while(i<s.length() && Character.isDigit(s.charAt(i))){
-            int digit = s.charAt(i) - '0';
-            if(res>Integer.MAX_VALUE/10 || res == Integer.MAX_VALUE/10 && digit > (sign == 1 ? 7 : 8)) return sign==1 ? Integer.MAX_VALUE: Integer.MIN_VALUE;
-            res = res*10 + digit;
+        while(i<n && Character.isDigit(s.charAt(i))){
+            int digit= s.charAt(i) - '0';
+            if(ans > Integer.MAX_VALUE/10 || ans == Integer.MAX_VALUE/10 && digit > (sign == 1 ? 7: 8)) return sign==1 ? Integer.MAX_VALUE: Integer.MIN_VALUE;
+            ans = ans*10 + digit;
             i++;
         }
-        return (int)(sign*res);
+        return (int)(sign * ans);
     }
 }
