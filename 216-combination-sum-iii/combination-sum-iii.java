8bfddcfb76b1f89e2954866f6c8898c74d3afe91
sankalp729 <sankalp7479@gmail.com>
2026-03-26 18:51:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (23.02%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,23 +1,18 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> ans = new ArrayList<>();
-        
-        int[] choices = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        backtrack(ans, new ArrayList<>(), choices, k, n, 0);
-        return ans;
+        List<List<Integer>> list= new ArrayList<>();
+        List<Integer> temp= new ArrayList<>();
+        rec(list, temp, k, n, 0, 1);
+        return list;
     }
-    public void backtrack(List<List<Integer>> ans, List<Integer> temp, int[] arr, int k, int n, int idx){
-        if(idx == arr.length || k==0){
-            if(n == 0){
-                if(k==0) ans.add(new ArrayList<>(temp));
-            }
+    public void rec(List<List<Integer>> list, List<Integer> temp, int k, int n, int sum, int curr){
+        if(k == temp.size() && n == sum){
+            list.add(new ArrayList<>(temp));
             return;
         }
-        for(int i = idx; i<arr.length; i++){
-            if(arr[i] > n) break;
-            
-            temp.add(arr[i]);
-            backtrack(ans, temp, arr, k-1, n-arr[i], i+1);
+        for(int i=curr; i<=9; i++){
+            temp.add(i);
+            rec(list, temp, k, n, sum+i, i+1);
             temp.remove(temp.size() - 1);
         }
     }
