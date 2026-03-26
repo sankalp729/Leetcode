4bf9caf2d6a164ab5a29466a6de9b0f4a2228d6e
sankalp729 <sankalp7479@gmail.com>
2026-03-26 12:25:00 +0530

    Time: 1 ms (99.89%) | Memory: 43.6 MB (92.54%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,25 +1,29 @@
 class Solution {
     public int myAtoi(String s) {
-        long ans= 0;
         int i=0;
-        int sign= 1;
-        int n= s.length();
-        while(i<n && s.charAt(i)== ' '){
+        int sign = 1;
+        long ans = 0;
+        while(i<s.length() && s.charAt(i) == ' '){
             i++;
         }
-        if(i<n && s.charAt(i) == '-'){
+        if(i<s.length() && s.charAt(i) == '-'){
             sign = -1;
             i++;
-        }else if(i<n && s.charAt(i) == '+'){
+        }
+        else if(i<s.length() && s.charAt(i) == '+'){
             sign = 1;
             i++;
         }
-        while(i<n && Character.isDigit(s.charAt(i))){
-            int digit= s.charAt(i) - '0';
-            if(ans > Integer.MAX_VALUE/10 || ans == Integer.MAX_VALUE/10 && digit > (sign == 1 ? 7: 8)) return sign==1 ? Integer.MAX_VALUE: Integer.MIN_VALUE;
-            ans = ans*10 + digit;
-            i++;
+        return parse(s, sign, i, ans);
+    }
+    public int parse(String s, int sign, int i, long ans){
+        if(i == s.length() || !Character.isDigit(s.charAt(i))){
+            return (int)(sign * ans);
         }
-        return (int)(sign * ans);
+        if(ans > Integer.MAX_VALUE/10 || ans == Integer.MAX_VALUE/10 && s.charAt(i) - '0' > (sign == 1 ? 7: 8)){
+            return (sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE);
+        }
+        ans = ans*10 + s.charAt(i) - '0';
+        return parse(s, sign, i+1, ans);
     }
 }
