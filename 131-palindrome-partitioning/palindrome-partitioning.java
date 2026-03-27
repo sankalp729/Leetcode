30c118f1b6dbab9d1dea58ab6c36c805059f5823
sankalp729 <sankalp7479@gmail.com>
2026-03-27 17:45:09 +0530

    Time: 9 ms (24.99%) | Memory: 65.1 MB (83.40%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,25 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s, int start, int end){
-        for(int i = start, j = end; i<=j; i++, j--){
+        for(int i=start, j=end; i<j; i++, j--){
             if(s.charAt(i) != s.charAt(j)) return false;
         }
         return true;
     }
     public List<List<String>> partition(String s) {
-        List<List<String>> ans = new ArrayList<>();
-        backtrack(ans, new ArrayList<>(), s, 0);
-        return ans;
+        List<List<String>> list= new ArrayList<>();
+        List<String> temp= new ArrayList<>();
+        rec(s, list, temp, 0);
+        return list;
     }
-    public void backtrack(List<List<String>> ans, List<String> temp, String s, int idx){
+    public void rec(String s, List<List<String>> list, List<String> temp, int idx){
         if(idx == s.length()){
-            ans.add(new ArrayList<>(temp));
+            list.add(new ArrayList<>(temp));
             return;
         }
-        for(int i = idx; i<s.length(); i++){
+        for(int i=idx; i<s.length(); i++){
             if(isPalindrome(s, idx, i)){
                 temp.add(s.substring(idx, i+1));
-                backtrack(ans, temp, s, i+1);
-                temp.remove(temp.size() - 1);
+                rec(s, list, temp, i+1);
+                temp.remove(temp.size() -1);
             }
         }
     }
