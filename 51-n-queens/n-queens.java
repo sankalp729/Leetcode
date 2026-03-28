ca81ce1e7708315d3d0af4de5c264cfeb3ac4f27
sankalp729 <sankalp7479@gmail.com>
2026-03-28 17:58:25 +0530

    Time: 2 ms (82.19%) | Memory: 46.4 MB (88.04%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,38 +1,56 @@
 class Solution {
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> res = new ArrayList<>();
+    public boolean isSafe(int row, int col, int n, char[][] board){
+        int origRow = row, origCol = col;
+        while(row>=0 && col>=0){
+            if(board[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
 
+        row= origRow;
+        col= origCol;
+        while(col>=0){
+            if(board[row][col] == 'Q') return false;
+            col--;
+        }
+
+        row= origRow;
+        col= origCol;
+        while(col>=0 && row<n){
+            if(board[row][col] == 'Q') return false;
+            col--;
+            row++;
+        }
+        return true;
+    }
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> list= new ArrayList<>();
+        List<String> temp= new ArrayList<>();
         char[][] board = new char[n][n];
-        for(int i =0; i<n; i++){
+        for(int i=0; i<n; i++){
             Arrays.fill(board[i], '.');
         }
-        boolean[] leftRow = new boolean[n];
-        boolean[] diag1 = new boolean[2*n - 1];
-        boolean[] diag2 = new boolean[2*n - 1];
-
-        backtrack(res, leftRow, diag1, diag2, board, n, 0);
-
-        return res;
+        rec(list, temp, n, 0, board);
+        return list;
     }
-    public void backtrack(List<List<String>> res, boolean[] leftRow, boolean[] diag1, boolean[] diag2, char[][] board, int n, int col){
+    public void rec(List<List<String>> list, List<String> temp, int n, int col, char[][] board){
         if(col == n){
-            List<String> temp = new ArrayList<>();
-            for(char[] ch : board){
-                temp.add(new String(ch));
-            }
-            res.add(new ArrayList<>(temp));
+            list.add(construct(board));
             return;
         }
-        for(int row =0; row < n; row++){
-            if(leftRow[row] || diag1[row + col] || diag2[row - col + n - 1]) continue;
-
-            leftRow[row] = diag1[row+col] = diag2[row-col+n-1] = true;
-            board[row][col] = 'Q';
-
-            backtrack(res, leftRow, diag1, diag2, board, n, col+1);
-
-            leftRow[row] = diag1[row+col] = diag2[row-col+n-1] = false;
-            board[row][col] = '.';
+        for(int row=0; row<n; row++){
+            if(isSafe(row, col, n, board)){
+                board[row][col] = 'Q';
+                rec(list, temp, n, col+1, board);
+                board[row][col] = '.';
+            }
         }
+    }
+    public List<String> construct(char[][] board){
+        List<String> temp= new ArrayList<>();
+        for(int i=0; i<board.length; i++){
+            temp.add(new String(board[i]));
+        }
+        return temp;
     }
 }
