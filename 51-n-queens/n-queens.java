f43cabdd05aec985dd05384a199aac8d4d97fb85
sankalp729 <sankalp7479@gmail.com>
2026-03-28 18:11:06 +0530

    Time: 2 ms (82.19%) | Memory: 46.5 MB (88.04%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,39 +1,40 @@
 class Solution {
     public boolean isSafe(int row, int col, int n, char[][] board){
-        int origRow = row, origCol = col;
+        int origRow = row;
+        int origCol = col;
+
         while(row>=0 && col>=0){
             if(board[row][col] == 'Q') return false;
             row--;
             col--;
         }
 
-        row= origRow;
+        row = origRow;
         col= origCol;
         while(col>=0){
             if(board[row][col] == 'Q') return false;
             col--;
         }
-
-        row= origRow;
-        col= origCol;
+        row = origRow;
+        col = origCol;
         while(col>=0 && row<n){
             if(board[row][col] == 'Q') return false;
             col--;
             row++;
         }
+
         return true;
     }
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> list= new ArrayList<>();
-        List<String> temp= new ArrayList<>();
-        char[][] board = new char[n][n];
+        char[][] board= new char[n][n];
         for(int i=0; i<n; i++){
             Arrays.fill(board[i], '.');
         }
-        rec(list, temp, n, 0, board);
+        rec(n, board, list, 0);
         return list;
     }
-    public void rec(List<List<String>> list, List<String> temp, int n, int col, char[][] board){
+    public void rec(int n, char[][] board, List<List<String>> list, int col){
         if(col == n){
             list.add(construct(board));
             return;
@@ -41,7 +42,7 @@ class Solution {
         for(int row=0; row<n; row++){
             if(isSafe(row, col, n, board)){
                 board[row][col] = 'Q';
-                rec(list, temp, n, col+1, board);
+                rec(n, board, list, col+1);
                 board[row][col] = '.';
             }
         }
