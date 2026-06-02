31a32e1d18846fc412c7777136b3208c2feb2c07
sankalp729 <sankalp7479@gmail.com>
2026-06-02 12:32:43 +0530

    Time: 4 ms (44.11%) | Memory: 50.8 MB (98.95%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,21 +1,20 @@
 class Solution {
-    public void dfs(char[][] grid, int row, int col){
-        grid[row][col] = '0';
-        int[] delrow = {-1, 0, 1, 0};
-        int[] delcol = {0, 1, 0, -1};
-
-        for(int i=0; i<4; i++){
-            int nrow = row+delrow[i];
-            int ncol = col+delcol[i];
-
-            if(nrow>=0 && nrow<grid.length && ncol>=0 && ncol<grid[0].length && grid[nrow][ncol] == '1'){
+    public void dfs(char[][] grid, int i, int j){
+        int n= grid.length, m= grid[0].length;
+        grid[i][j] = '0';
+        int[] delrow= {-1, 0, 1, 0};
+        int[] delcol= {0, 1, 0, -1};
+        for(int k=0; k<4; k++){
+            int nrow= i+delrow[k];
+            int ncol= j+delcol[k];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol] == '1'){
                 dfs(grid, nrow, ncol);
             }
         }
     }
     public int numIslands(char[][] grid) {
-        int n= grid.length, m= grid[0].length;
         int cnt= 0;
+        int n= grid.length, m= grid[0].length;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j] == '1'){
