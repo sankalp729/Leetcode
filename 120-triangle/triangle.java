5b6851123b597f68df2e7cbafb93b4aadfe985f7
sankalp729 <sankalp7479@gmail.com>
2026-06-28 13:34:09 +0530

    Time: 8 ms (16.65%) | Memory: 46.4 MB (18.27%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,26 +1,19 @@
 class Solution {
-    public int rec(int i, int j, List<List<Integer>> triangle, int n, List<List<Integer>> dp){
-        if(i == n-1) return triangle.get(i).get(j);
-
-        if(dp.get(i).get(j) != null) return dp.get(i).get(j);
-
-        int down = triangle.get(i).get(j) + rec(i+1, j, triangle, n, dp);
-        int diag = triangle.get(i).get(j) + rec(i+1, j+1, triangle, n, dp);
-
-        int ans = Math.min(down, diag);
-        dp.get(i).set(j, ans);
-        return ans;
-    }
     public int minimumTotal(List<List<Integer>> triangle) {
+        List<Integer> front = new ArrayList<>();
         int n = triangle.size();
-        List<List<Integer>> dp = new ArrayList<>();
         for(int i=0; i<n; i++){
-            List<Integer> row = new ArrayList<>();
-            for(int j=0; j<=i; j++){
-                row.add(null);
-            }
-            dp.add(row);
+            front.add(triangle.get(n-1).get(i));
         }
-        return rec(0, 0 , triangle, n, dp);
+        for(int i=n-2; i>=0; i--){
+            List<Integer> curr = new ArrayList<>();
+            for(int j=0; j<=i; j++){
+                int d = triangle.get(i).get(j) + front.get(j);
+                int dg = triangle.get(i).get(j) + front.get(j+1);
+                curr.add(Math.min(d, dg));
+            }
+            front = curr;
+        }
+        return front.get(0);
     }
 }
