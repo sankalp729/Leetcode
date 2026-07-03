6d2e17aa10341752409762e0d868387d6711acbe
sankalp729 <sankalp7479@gmail.com>
2026-07-03 17:21:13 +0530

    Time: 22 ms (44.26%) | Memory: 54 MB (34.86%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,29 +1,30 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int idx1 = text1.length()-1, idx2= text2.length()-1;
+        int idx1 = text1.length(), idx2= text2.length();
         int[][] dp = new int[idx1+1][idx2+1];
-        if(text1.charAt(0) == text2.charAt(0)) dp[0][0] = 1;
-        for(int i=1; i<=idx1; i++){
-            if(text1.charAt(i) == text2.charAt(0)){
-                dp[i][0] = 1;
-            }else{
-                dp[i][0] = dp[i-1][0];
-            }
-        }
-        for(int i=1; i<=idx2; i++){
-            if(text1.charAt(0) == text2.charAt(i)){
-                dp[0][i] = 1;
-            }else{
-                dp[0][i] = dp[0][i-1];
-            }
-        }
         for(int i=1; i<=idx1; i++){
             for(int j=1; j<=idx2; j++){
-                if(text1.charAt(i) == text2.charAt(j)) dp[i][j] = 1+dp[i-1][j-1];
-
-                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
+        StringBuilder sb = new StringBuilder();
+        int i =idx1, j =idx2;
+        while(i>0 && j>0){
+            if(text1.charAt(i-1) == text2.charAt(j-1)){
+                sb.append(text1.charAt(i-1));
+                i--;
+                j--;
+            }else if(dp[i-1][j] >= dp[i][j-1]){
+                i--;
+            }else{
+                j--;
+            }
+        }
+        System.out.println(sb.reverse());
         return dp[idx1][idx2];
     }
 }
