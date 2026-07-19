d2273e283d8a0b42c1109e585d0ae915ee98ecdc
sankalp729 <sankalp7479@gmail.com>
2026-07-19 10:01:02 +0530

    Time: 6 ms (99.63%) | Memory: 47.7 MB (87.71%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,22 +1,24 @@
 class Solution {
-    public long isPossible(int[] piles, int h, int mid){
-        long sum = 0;
-        for(int i =0; i<piles.length; i++){
-            sum+= (piles[i] + mid - 1)/ mid;
-            if(sum > h) return sum;
+    public boolean isPossible(int[] piles, int h, int mid){
+        int sum = 0;
+        for(int i=0; i<piles.length; i++){
+            sum+= (piles[i] + mid-1)/mid;
+            if(sum>h) return false;
         }
-        return sum;
+        return sum<=h;
     }
     public int minEatingSpeed(int[] piles, int h) {
-        int low = 1;
-        int high = piles[0];
-        for(int i: piles){
-            high = Math.max(high, i);
-        }
+        int low = 1, high = Integer.MIN_VALUE;
+        for(int i=0; i<piles.length; i++){
+            high = Math.max(high, piles[i]);
+        } 
         while(low<=high){
-            int mid = (low + high)/2;
-            if(isPossible(piles, h, mid) > h) low = mid+1;
-            else high = mid-1;
+            int mid=(low+high)/2;
+            if(isPossible(piles, h, mid)){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
         }
         return low;
     }
